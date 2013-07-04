c02e8b06c575de365ea9a2a30181cf7d5a728151
Richard Hancock <ygrichman@mail.com>
2013-07-04 16:44:42 +0100

    Added Google Analytics

--- quiz.php
+++ quiz.php
@@ -7,6 +7,18 @@
 		<script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
 		<script src="js/background.js" type="text/javascript"></script>
 		<meta charset="UTF-8">
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-36113290-1']);
+			_gaq.push(['_setDomainName', 'spacecrazyproductions.com']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  	})();
+		</script>
 	</head>
 	<body>
 		<?php
